ea5629e2f938db9a0cba0a94dd1309be6f0db132
Andy Boothe <andy.boothe@gmail.com>
2022-08-29 14:42:34 -0500

    add jakarta.servlet package implementation

---
+++ httpmodel-jakarta-servlet/java/com/sigpwned/httpmodel/servlet/util/ModelHttpServlets.java
@@ -0,0 +1,143 @@
+/*-
+ * =================================LICENSE_START==================================
+ * httpmodel-servlet
+ * ====================================SECTION=====================================
+ * Copyright (C) 2022 Andy Boothe
+ * ====================================SECTION=====================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==================================LICENSE_END===================================
+ */
+package com.sigpwned.httpmodel.servlet.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.sigpwned.httpmodel.ModelHttpAuthority;
+import com.sigpwned.httpmodel.ModelHttpEntity;
+import com.sigpwned.httpmodel.ModelHttpHeaders;
+import com.sigpwned.httpmodel.ModelHttpHost;
+import com.sigpwned.httpmodel.ModelHttpMediaType;
+import com.sigpwned.httpmodel.ModelHttpQueryString;
+import com.sigpwned.httpmodel.ModelHttpRequest;
+import com.sigpwned.httpmodel.ModelHttpResponse;
+import com.sigpwned.httpmodel.ModelHttpUrl;
+import com.sigpwned.httpmodel.util.ModelHttpHeaderNames;
+import com.sigpwned.httpmodel.util.ModelHttpMediaTypes;
+import com.sigpwned.httpmodel.util.MoreByteStreams;
+
+/**
+ * This is server side.
+ */
+public final class ModelHttpServlets {
+  private ModelHttpServlets() {}
+
+  /**
+   * Converts the given {@link HttpServletRequest} into a {@link ModelHttpRequest}.
+   */
+  public static ModelHttpRequest fromRequest(HttpServletRequest request) throws IOException {
+    ModelHttpQueryString queryString = Optional.ofNullable(request.getQueryString())
+        .map(ModelHttpQueryString::fromString).orElse(null);
+
+    ModelHttpUrl url = ModelHttpUrl.of(
+        request.getScheme(), ModelHttpAuthority
+            .of(ModelHttpHost.fromString(request.getServerName()), request.getServerPort()),
+        request.getRequestURI(), queryString);
+
+    String method = request.getMethod().toUpperCase();
+
+    String version = request.getProtocol();
+
+    List<ModelHttpHeaders.Header> headers = new ArrayList<>();
+
+    Enumeration<String> headerNames = request.getHeaderNames();
+    while (headerNames.hasMoreElements()) {
+      String headerName = headerNames.nextElement();
+      Enumeration<String> headerValues = request.getHeaders(headerName);
+      while (headerValues.hasMoreElements()) {
+        String headerValue = headerValues.nextElement();
+        headers.add(ModelHttpHeaders.Header.of(headerName.toLowerCase(), headerValue));
+      }
+    }
+
+    ModelHttpHeaders.Header contentTypeHeader =
+        headers.stream().filter(h -> h.getName().equals(ModelHttpHeaderNames.CONTENT_TYPE))
+            .findFirst().orElse(null);
+    ModelHttpHeaders.Header contentLengthHeader =
+        headers.stream().filter(h -> h.getName().equals(ModelHttpHeaderNames.CONTENT_LENGTH))
+            .findFirst().orElse(null);
+    ModelHttpHeaders.Header transferEncodingHeader =
+        headers.stream().filter(h -> h.getName().equals(ModelHttpHeaderNames.TRANSFER_ENCODING))
+            .findFirst().orElse(null);
+
+    ModelHttpEntity entity;
+    if (contentTypeHeader != null || contentLengthHeader != null
+        || transferEncodingHeader != null) {
+      ModelHttpMediaType type =
+          headers.stream().filter(h -> h.getName().equals(ModelHttpHeaderNames.CONTENT_TYPE))
+              .map(ModelHttpHeaders.Header::getValue).map(ModelHttpMediaType::fromString)
+              .findFirst().orElse(null);
+
+      byte[] entityBody;
+      try (InputStream in = request.getInputStream()) {
+        entityBody = MoreByteStreams.toByteArray(in);
+      }
+
+      entity = ModelHttpEntity.of(
+          Optional.ofNullable(type).orElse(ModelHttpMediaTypes.APPLICATION_OCTET_STREAM),
+          entityBody);
+    } else {
+      entity = null;
+    }
+
+    return ModelHttpRequest.of(version, method, url, ModelHttpHeaders.of(headers), entity);
+  }
+
+  /**
+   * Fills in the given {@link HttpServletResponse} from the given {@link ModelHttpResponse}
+   * 
+   * @throws IOException
+   */
+  public static HttpServletResponse toResponse(HttpServletResponse result,
+      ModelHttpResponse response) throws IOException {
+    result.setStatus(response.getStatusCode());
+
+    for (ModelHttpHeaders.Header header : response.getHeaders()) {
+      // TODO Encode headers?
+      // See: https://stackoverflow.com/questions/324470/http-headers-encoding-decoding-in-java
+      result.addHeader(header.getName(), header.getValue());
+    }
+
+    if (response.getEntity().isPresent()) {
+      ModelHttpEntity entity = response.getEntity().get();
+
+      result.setContentType(
+          entity.getType().orElse(ModelHttpMediaTypes.APPLICATION_OCTET_STREAM).toString());
+
+      result.setContentLength(entity.length());
+
+      try (OutputStream out = result.getOutputStream()) {
+        try (InputStream in = entity.readBytes()) {
+          MoreByteStreams.drain(in, out);
+        }
+      }
+    }
+
+    return result;
+  }
+}
---
+++ httpmodel-jakarta-servlet/src/test/java/com/sigpwned/httpmodel/jakarta/servlet/util/ModelHttpServletsTest.java
@@ -0,0 +1,99 @@
+/*-
+ * =================================LICENSE_START==================================
+ * httpmodel-servlet
+ * ====================================SECTION=====================================
+ * Copyright (C) 2022 Andy Boothe
+ * ====================================SECTION=====================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==================================LICENSE_END===================================
+ */
+package com.sigpwned.httpmodel.jakarta.servlet.util;
+
+import static java.util.Collections.emptyEnumeration;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import org.junit.Test;
+import com.sigpwned.httpmodel.ModelHttpEntity;
+import com.sigpwned.httpmodel.ModelHttpRequest;
+import com.sigpwned.httpmodel.ModelHttpResponse;
+import com.sigpwned.httpmodel.ModelHttpUrl;
+import com.sigpwned.httpmodel.util.ModelHttpMediaTypes;
+import com.sigpwned.httpmodel.util.ModelHttpMethods;
+import com.sigpwned.httpmodel.util.ModelHttpStatusCodes;
+import com.sigpwned.httpmodel.util.ModelHttpVersions;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.WriteListener;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ModelHttpServletsTest {
+  @Test
+  public void shouldConvertGetRequestProperly() throws IOException {
+    final HttpServletRequest req = mock(HttpServletRequest.class);
+    when(req.getScheme()).thenReturn("http");
+    when(req.getProtocol()).thenReturn("HTTP/1.1");
+    when(req.getMethod()).thenReturn("GET");
+    when(req.getServerName()).thenReturn("localhost");
+    when(req.getServerPort()).thenReturn(8080);
+    when(req.getRequestURI()).thenReturn("/path/to/example");
+    when(req.getHeaderNames()).thenReturn(emptyEnumeration());
+
+    ModelHttpRequest request = ModelHttpServlets.fromRequest(req);
+
+    assertThat(request, is(ModelHttpRequest.of(ModelHttpVersions.HTTP_1_1, ModelHttpMethods.GET,
+        ModelHttpUrl.fromString("http://localhost:8080/path/to/example"), Optional.empty())));
+  }
+
+  @Test
+  public void shouldConvertResponseProperly() throws IOException {
+    byte[] bytes = "hello".getBytes(StandardCharsets.UTF_8);
+
+    ModelHttpResponse response = ModelHttpResponse.of(ModelHttpStatusCodes.OK,
+        ModelHttpEntity.of(ModelHttpMediaTypes.TEXT_PLAIN, bytes));
+
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    final HttpServletResponse res = mock(HttpServletResponse.class);
+    when(res.getOutputStream()).thenReturn(new ServletOutputStream() {
+      @Override
+      public boolean isReady() {
+        return true;
+      }
+
+      @Override
+      public void setWriteListener(WriteListener writeListener) {
+        // NOP
+      }
+
+      @Override
+      public void write(int b) throws IOException {
+        out.write(b);
+      }
+    });
+
+    ModelHttpServlets.toResponse(res, response);
+
+    verify(res).setContentType(ModelHttpMediaTypes.TEXT_PLAIN.toString());
+    verify(res).setContentLength(bytes.length);
+    verify(res).setStatus(ModelHttpStatusCodes.OK);
+
+    assertThat(out.toByteArray(), is(bytes));
+  }
+}
