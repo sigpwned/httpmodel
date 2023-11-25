6d93ccf0ac175361f144987a62b8c382148cd6ce
Andy Boothe <andy.boothe@gmail.com>
2023-11-24 20:50:42 -0600

    fix javadoc error

--- httpmodel-core/src/main/java/com/sigpwned/httpmodel/core/io/BufferedInputStream.java
+++ httpmodel-core/src/main/java/com/sigpwned/httpmodel/core/io/BufferedInputStream.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -52,7 +52,6 @@ public abstract class BufferedInputStream extends FilterInputStream {
   /**
    * hook
    *
-   * @param file
    * @return An {@link InputStream} containing the same bytes as the original argument to the
    *         constructor. Must not be {@code null}.
    * @throws FileNotFoundException
